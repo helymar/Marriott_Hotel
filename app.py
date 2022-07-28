a48bca3d3ab8de21bc385881e3c5006076213971
WilderBello <wobelloh@unal.edu.co>
2022-07-27 22:29:35 -0500

    add login, signup and home

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/home')
 def index():
     return render_template('index.html')
 
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
